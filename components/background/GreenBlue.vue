938106be0dcb6dd8b860229c4e4a97e13bb80805
asyraf-adianto <asyraf.adianto@gmail.com>
2021-12-18 23:46:49 +0700

    try

---
+++ components/background/GreenBlue.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="fixed -z-50 flex items-top justify-center min-h-screen bg-gradient-to-br from-green-500 to-indigo-500 w-screen h-screen">
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        zIndex: {
+         '-50': '-50',
+        }
+      }
+    }
+  }
