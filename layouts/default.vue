2c5dc90b1028274cbe5e662ce3befddc527129a8
asyraf-adianto <asyraf.adianto@gmail.com>
2021-12-20 01:38:13 +0700

    Add Project Support System in Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,8 +31,26 @@
         </div>
 
         <!-- Footer Container-->
-        <div class="flex-none flex flex-row items-center justify-right h-10 border-t border-gray-500">
+        <div class="flex-none flex flex-row items-center justify-end h-10 border-t border-gray-500 gap-3">
           <p class="text-gray-500">This project cannot be developed without using:</p>
+          <a href="https://nuxtjs.org/">
+            <img src="/nuxt-js-modified.svg" class="h-6"/>
+          </a>
+          <a href="https://tailwindcss.com/">
+            <img src="/tailwind-css-modified.svg" class="h-6"/>
+          </a>
+          <a href="https://github.com/">
+            <img src="/github-modified.svg" class="h-6"/>
+          </a>
+          <a href="https://vercel.com/">
+            <img src="/vercel-modified.svg" class="h-6"/>
+          </a>
+          <a href="https://cloudinary.com/">
+            <img src="/cloudinary-modified.svg" class="h-6"/>
+          </a>
+          <a href="https://www.deta.sh/">
+            <img src="/deta-cloud-modified.svg" class="h-6"/>
+          </a>
         </div>
       </div>
     </div>
