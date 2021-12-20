dd354339551a29de6ef2d02d8af4fde228e51852
asyraf-adianto <asyraf.adianto@gmail.com>
2021-12-20 07:44:53 +0700

    add image

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full h-full flex flex-col">
       <div class="flex-none bg-green-200 h-full flex flex-col text-center xl:flex-row xl:test-left">
-          Asyraf N. Adianto
+          <img src="https://res.cloudinary.com/dsipw0z3u/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1639960232/profile/62_822-3480-5590_20211220_072758_z1xued.jpg" class="h-1/2" />
+          <p class="text-3xl">Asyraf N. Adianto</p>
+      
       </div>
       <div class="flex-none bg-blue-200 h-full flex flex-col text-center xl:flex-row xl:test-left">
           Project Management
