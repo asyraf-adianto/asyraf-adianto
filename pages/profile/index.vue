c625b23144043bb75e8072760dc992ebd38d23b8
asyraf-adianto <asyraf.adianto@gmail.com>
2021-12-20 08:06:58 +0700

    mod image

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="w-full h-full flex flex-col">
       <div class="flex-none bg-green-200 h-full flex flex-col text-center xl:flex-row xl:test-left">
-          <div class="aspect-square h-1/4 rounded-full object-cover">
+          <div class="aspect-square h-52 rounded-full object-cover">
             <img 
               src="https://res.cloudinary.com/dsipw0z3u/image/upload/ar_1:1,c_fill,g_auto,o_100,q_100,r_max,w_640,z_1.1/v1639960232/profile/62_822-3480-5590_20211220_072758_z1xued.png" 
             />
           </div>
-          <p class="text-3xl">Asyraf N. Adianto</p>
+          <p class="text-3xl">
+              Asyraf N. Adianto
+          </p>
       
       </div>
       <div class="flex-none bg-blue-200 h-full flex flex-col text-center xl:flex-row xl:test-left">
